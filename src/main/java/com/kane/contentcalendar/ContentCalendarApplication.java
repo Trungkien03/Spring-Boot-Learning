c9d901abcf88f384b11d23aa84042536800a9e38
Kane03 <trungkiennguyen0310@gmail.com>
2024-04-01 17:01:28 +0700

    get bean definition Names

--- src/main/java/com/kane/contentcalendar/ContentCalendarApplication.java
+++ src/main/java/com/kane/contentcalendar/ContentCalendarApplication.java
@@ -2,12 +2,16 @@ package com.kane.contentcalendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ContentCalendarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContentCalendarApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ContentCalendarApplication.class, args);
+		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
 }
